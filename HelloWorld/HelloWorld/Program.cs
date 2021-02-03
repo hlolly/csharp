0b3145ca50eafcbcb585f9f22f1ce2d9df3c0057
Patrícia Faria <63451914+hlolly@users.noreply.github.com>
2021-02-03 14:12:39 +0100

    Add files via upload

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            string myname = "Patricia";
+            string message = "My name is " + myname;
+
+            string capsMessage = message.ToUpper();
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(capsMessage);
+
+            Console.Read();
+            
+
+        }
+    }
+}
